09556b856d0bd4d0ab10840ed2cb5d0080fe1ed1
Hibathulla <hibathullacm@gmail.com>
2023-10-02 23:57:57 +0530

    refactor(geocode): geocode type error fix

--- src/lib/geocode.tsx
+++ src/lib/geocode.tsx
@@ -15,7 +15,7 @@ export const getGeocodeAddress = (address: string) => {
   Geocode.fromAddress(address).then(
     (response) => {
       const { lat, lng } = response.results[0].geometry.location;
-      lat, lng;
+
       loc = lat;
     },
     (error) => {
