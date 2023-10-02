0cc4e02432d2569af7d1c91397d42d8b2a5b9583
Hibathulla <hibathullacm@gmail.com>
2023-10-02 23:52:40 +0530

    refactor(type): type error fix

--- axios-config.ts
+++ axios-config.ts
@@ -6,8 +6,6 @@ export const axiosInstance = axios.create({
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
-    config, "config";
-
     const token = localStorage.getItem("token");
     if (token) {
       config.headers["Authorization"] = "Bearer " + token;
