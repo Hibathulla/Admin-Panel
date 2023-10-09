57b6016e7d5c20622cf8bafdbec362b9fed54970
Hibathulla <hibathullacm@gmail.com>
2023-10-09 23:30:04 +0530

    refactor(domains): new domains added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "ecommerce-27ee.onrender.com"],
+    domains: ["localhost", "ecommerce-api-depn.onrender.com", "127.0.0.1"],
   },
 };
 
